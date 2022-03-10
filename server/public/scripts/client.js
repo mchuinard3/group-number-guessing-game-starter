9bb451665d90e1f268e569c93ae7d1a1bf1703fb
Michael Chuinard <mpchuinard@finlandia.edu>
2022-03-10 14:57:37 -0600

    completed server portion of assignment

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,49 @@ $(document).ready(handleReady);
 
 function handleReady() {
   console.log("jquery is loaded!")
+  $(`#submit`).on(`click`, handleSubmit);
 }
+
+function handleSubmit (){
+  let playerOne = $(`#playerOne`).val();
+  let playerTwo = $(`#playerTwo`).val(); 
+  let playerThree = $(`#playerThree`).val();
+  let playerFour = $(`#playerFour`).val();
+
+  $.ajax({
+    url: '/guesses',
+    method: 'POST',
+    data: {
+      guessPlayerOne: playerOne,
+      guessPlayerTwo: playerTwo,
+      guessPlayerThree: playerThree,
+      guessPlayerFour: playerFour
+    }
+  }).then(function (response) {
+    console.log(response);
+    
+    $('#playerOne').val('')
+    $(`#playerTwo`).val('');
+    $(`#playerThree`).val('');
+    $(`#playerFour`).val('');
+
+  })
+
+  getGuesses();
+  
+}
+
+function getGuesses() {
+
+  $.ajax({
+    url: '/guesses',
+    method: 'GET',
+  }).then(function (response) {
+    console.log(response);
+    
+  })
+}
+
+
+
+
--- server/server.js
+++ server/server.js
@@ -3,6 +3,13 @@ const bodyParser = require('body-parser')
 const app = express();
 const PORT = 5000;
 
+let allGuesses = {
+  playerOneGuess: [],
+  playerTwoGuess: [],
+  playerThreeGuess: [],
+  playerFourGuess: []
+};
+
 // This must be added before GET & POST routes.
 app.use(bodyParser.urlencoded({extended:true}))
 
@@ -10,8 +17,33 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static('server/public'));
 
 // GET & POST Routes go here
+app.post(`/guesses`, (req, res) => {
+  console.log(`POST guesses`, req.body);
+  let dummy = req.body; 
+  // // input values
 
+  allGuesses.playerOneGuess.push(dummy.guessPlayerOne);
+  allGuesses.playerTwoGuess.push(dummy.guessPlayerTwo);
+  allGuesses.playerThreeGuess.push(dummy.guessPlayerThree);
+  allGuesses.playerFourGuess.push(dummy.guessPlayerFour);
+
+  // console.log(playerOneGuess);
+  // console.log(playerTwoGuess);
+  // console.log(playerThreeGuess);
+  // console.log(playerFourGuess);
+
+  console.log(allGuesses);
+  
+  res.sendStatus(201);
+})
+
+app.get('/guesses', (req, res) => {
+  console.log('GET guesses');
+  res.send(allGuesses);
+})
 
 app.listen(PORT, () => {
   console.log ('Server is running on port', PORT)
 })
+
+
