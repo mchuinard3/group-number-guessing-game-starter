0967f1d09b70a123b67abc12472dbe36cf003be3
Michael Chuinard <mpchuinard@finlandia.edu>
2022-03-10 16:43:38 -0600

    GroupNoGuessProgress

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,13 +1,15 @@
 $(document).ready(handleReady);
 
+let randomNum = 0;
 function handleReady() {
   console.log("jquery is loaded!")
   $(`#submit`).on(`click`, handleSubmit);
+  randomNum = randomNumber(1, 25);
 }
 
-function handleSubmit (){
+function handleSubmit() {
   let playerOne = $(`#playerOne`).val();
-  let playerTwo = $(`#playerTwo`).val(); 
+  let playerTwo = $(`#playerTwo`).val();
   let playerThree = $(`#playerThree`).val();
   let playerFour = $(`#playerFour`).val();
 
@@ -22,16 +24,16 @@ function handleSubmit (){
     }
   }).then(function (response) {
     console.log(response);
-    
+
     $('#playerOne').val('')
     $(`#playerTwo`).val('');
     $(`#playerThree`).val('');
     $(`#playerFour`).val('');
 
+    
   })
-
   getGuesses();
-  
+
 }
 
 function getGuesses() {
@@ -41,10 +43,38 @@ function getGuesses() {
     method: 'GET',
   }).then(function (response) {
     console.log(response);
-    
+    render(response);
   })
 }
 
+function checkGuesses(guesses, player) {
+  let hint = '';
+  for (let guess of guesses) {
+    if (guess > randomNum) {
+      hint = 'guess lower';
+    }
+    if (guess < randomNum) {
+      hint = 'guess higher';
+    }
+    if (guess === randomNum) {
+      hint = 'You guessed the correct number!!!';
+    }
+  }
+  $(player).append(`<li>Guess: ${guess} - ${hint}</li>`)
+}
 
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (1 + max - min) + min);
+}
 
-
+function render(guesses) {
+  let pOneGuesses = guesses.playerOneGuess;
+  let pTwoGuesses = guesses.playerTwoGuess;
+  let pThreeGuesses = guesses.playerThreeGuess;
+  let pFourGuesses = guesses.playerFourGuess;
+  
+  checkGuesses(pOneGuesses, '#playerOneGuessList')
+  checkGuesses(pTwoGuesses, '#playerTwoGuessList')
+  checkGuesses(pThreeGuesses, '#playerThreeGuessList')
+  checkGuesses(pFourGuesses, '#playerFourGuessList')
+}
